1aa404fd055d42e36a815f5fb89084f8b65c90fd
Ayushi Chaudhuri <61285140+IUC4801@users.noreply.github.com>
2023-09-19 00:40:51 +0530

    Added about section and completed homepage

---
+++ src/components/Skills.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { motion } from "framer-motion"
+import { Scada } from 'next/font/google'
+
+const Skill = ({ name, x, y }) => {
+    return (
+        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
+                py-3 px-6 shadow-dark cursor-pointer absolute '
+            whileHover={{ scale: 1.05 }}
+            initial={{ x: 0, y: 0 }}
+            whileInView={{ x: x, y: y }}
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+        >
+            {name}
+        </motion.div>
+
+    )
+}
+
+const Skills = () => {
+    return (
+        <>
+            <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
+            <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+                <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
+                p-8 shadow-dark cursor-pointer'
+                    whileHover={{ scale: 1.05 }}
+                >
+                    Security
+                </motion.div>
+
+                <Skill name="Burpsuite" x="-25vw" y="2vw" />
+                <Skill name="Firewall" x="-5vw" y="-10vw" />
+                <Skill name="Splunk" x="20vw" y="6vw" />
+                <Skill name="Snort" x="0vw" y="12vw" />
+                <Skill name="Python" x="-20vw" y="-15vw" />
+                <Skill name="Java" x="15vw" y="-12vw" />
+                <Skill name="ReactJS" x="32vw" y="-5vw" />
+                <Skill name="NextJS" x="0vw" y="-20vw" />
+                <Skill name="NodeJS" x="-25vw" y="18vw" />
+                <Skill name="Solidity" x="18vw" y="18vw" />
+            </div>
+        </>
+    )
+}
+
+export default Skills
